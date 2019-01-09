c7497276ac47f042e93334e229384dae28f0ed2d
gujitao001 <gujitao001@ke.com>
2019-01-09 15:20:10 +0800

    add files

---
+++ EasyShell/EasyShellTests/EasyShellTests.m
@@ -0,0 +1,37 @@
+//
+//  EasyShellTests.m
+//  EasyShellTests
+//
+//  Created by lianjia on 2019/1/9.
+//  Copyright © 2019 lianjia. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EasyShellTests : XCTestCase
+
+@end
+
+@implementation EasyShellTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ EasyShell/Pods/Target Support Files/Pods-EasyShellUITests/Pods-EasyShellUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EasyShellUITests : NSObject
+@end
+@implementation PodsDummy_Pods_EasyShellUITests
+@end
